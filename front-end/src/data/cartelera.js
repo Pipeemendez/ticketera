7bc1a6d57b7c100979d204660cc6cbba96ee9537
Felipe Méndez <94472095+Pipeemendez@users.noreply.github.com>
2025-10-22 18:23:26 -0300

    Create cartelera.js

---
+++ front-end/src/data/cartelera.js
@@ -0,0 +1,10 @@
+const eventos = [
+    {id:1, nombre:"Los Jaivas", fecha:"2023-10-15", lugar:"Estadio Nacional", precio:50},
+    {id:2, nombre:"Mon Laferte", fecha:"2023-11-20", lugar:"Teatro Caupolicán", precio:40},
+    {id:3, nombre:"Coldplay", fecha:"2024-01-10", lugar:"Estadio Monumental", precio:100},
+    {id:4, nombre:"BTS", fecha:"2024-02-25", lugar:"Movistar Arena", precio:120},
+    {id:5, nombre:"Shakira", fecha:"2024-03-15", lugar:"Estadio Nacional", precio:80},
+    {id:6, nombre:"Metallica", fecha:"2024-04-05", lugar:"Estadio Monumental", precio:110},
+]
+
+export default eventos;
