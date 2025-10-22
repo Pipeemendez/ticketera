46e72f1953823954d9533b59743cbc596f6c5ddf
Felipe Méndez <94472095+Pipeemendez@users.noreply.github.com>
2025-10-22 17:54:32 -0300

    Create Footer.jsx

---
+++ front-end/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+function Footer (){
+    return (
+        <footer className="footer">
+            <p className="footer-text">© 2024 Ticketera. All rights reserved.</p>
+        </footer>
+    )
+}
+
+export default Footer 
