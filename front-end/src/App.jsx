1ec546e5a71d23e84e376b40520c5d3a4391c57d
Felipe Méndez <94472095+Pipeemendez@users.noreply.github.com>
2025-10-22 17:48:40 -0300

    Update App.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
+import Navbar from "./components/Navbar";
 
 function App() {
 
   return (
     <>
-      <h1>Hello, World!</h1>
+      <Navbar />
     </>
   )
 }
