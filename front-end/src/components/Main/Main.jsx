07cdc68d421550e901eb113bb2ecd29fbf0eb0a5
Felipe Méndez <94472095+Pipeemendez@users.noreply.github.com>
2025-10-22 17:54:38 -0300

    Create Main.jsx

---
+++ front-end/src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import "./Main.css";
+
+function Main () {
+    return (
+        <main className="main">
+            <h2 className="main-title">Welcome to Ticketera</h2>
+            <p className="main-description">Your one-stop solution for event ticketing.</p>
+        </main>
+    )
+}
+
+export default Main;
