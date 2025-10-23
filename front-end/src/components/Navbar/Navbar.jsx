fe11b673ea41a6243bad43d797b8e39ba710c4d3
Felipe Méndez <94472095+Pipeemendez@users.noreply.github.com>
2025-10-23 18:47:47 -0300

    Update Navbar.jsx

--- front-end/src/components/Navbar/Navbar.jsx
+++ front-end/src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar () {
     return (
         <nav className="navbar">
             <div className='navbar-elemento'> Ticketera </div>
-            <div className='navbar-elemento'> Busqueda </div>
+            <div className='navbar-elemento'> Búsqueda </div>
             <div className='navbar-elemento'> Carrito </div>
         </nav>
     )
