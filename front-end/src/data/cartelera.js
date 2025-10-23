a38e74bfa2634cd827d78dfd922b2565c338043a
Felipe Méndez <94472095+Pipeemendez@users.noreply.github.com>
2025-10-23 18:11:12 -0300

    Update cartelera.js

--- front-end/src/data/cartelera.js
+++ front-end/src/data/cartelera.js
@@ -5,6 +5,9 @@ const eventos = [
     {id:4, nombre:"BTS", fecha:"2024-02-25", lugar:"Movistar Arena", precio:120},
     {id:5, nombre:"Shakira", fecha:"2024-03-15", lugar:"Estadio Nacional", precio:80},
     {id:6, nombre:"Metallica", fecha:"2024-04-05", lugar:"Estadio Monumental", precio:110},
+    {id:7, nombre:"Dua Lipa", fecha:"2024-05-20", lugar:"Teatro Caupolicán", precio:70},
+    {id:8, nombre:"The Weeknd", fecha:"2024-06-30", lugar:"Movistar Arena", precio:90},
+    {id:9, nombre:"Adele", fecha:"2024-07-25", lugar:"Estadio Nacional", precio:130},
 ]
 
 export default eventos;
