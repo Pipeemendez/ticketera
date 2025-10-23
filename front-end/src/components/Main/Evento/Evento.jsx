9a786c3362134f5cc54dd32fd501ceb17e7ce896
Felipe Méndez <94472095+Pipeemendez@users.noreply.github.com>
2025-10-23 18:31:55 -0300

    Update Evento.jsx

--- front-end/src/components/Main/Evento/Evento.jsx
+++ front-end/src/components/Main/Evento/Evento.jsx
@@ -1,10 +1,11 @@
-
+import "./Evento.css";
 
 function Evento ({evento}) {
     return (
         <li className="cartelera-item">
             <h4 className="cartelera-name"> {evento.nombre} </h4>
             <p className="cartelera-details"> {evento.fecha} | Lugar: {evento.lugar} | Precio: ${evento.precio} </p>
+            <button className="cartelera-button"> Ver más información </button>
         </li>   
     );
 }
