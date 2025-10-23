91ab97526fb8c950b1d514d5d18043de82141ea7
Felipe Méndez <94472095+Pipeemendez@users.noreply.github.com>
2025-10-23 18:47:18 -0300

    Update Main.jsx

--- front-end/src/components/Main/Main.jsx
+++ front-end/src/components/Main/Main.jsx
@@ -5,11 +5,11 @@ import Evento from "./Evento/Evento.jsx";
 function Main () {
     return (
         <main className="main">
-            <h2 className="main-title">Welcome to Ticketera</h2>
-            <p className="main-description">Your one-stop solution for event ticketing.</p>
+            <h2 className="main-title">Bienvenid@ a Ticketera</h2>
+            <p className="main-description">Tu solución integral para la venta de entradas para eventos.</p>
 
             <div className="cartelera">
-                <h3 className="cartelera-title">Upcoming Events</h3>
+                <h3 className="cartelera-title">Próximos Eventos</h3>
 
             <ul className="cartelera-list">
                 {
