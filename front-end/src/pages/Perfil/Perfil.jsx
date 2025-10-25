a3441da70b06ccaef23d98b1c25686d28d1e5b98
Felipe Méndez <94472095+Pipeemendez@users.noreply.github.com>
2025-10-25 18:36:59 -0300

    Update Perfil.jsx

---
+++ front-end/src/pages/Perfil/Perfil.jsx
@@ -0,0 +1,12 @@
+import './Perfil.css';
+
+function Perfil () {
+    return (
+        <div className="perfil">
+            <h2 className="perfil-title">Mi Perfil</h2>
+            <p className="perfil-description">Aquí puedes ver y editar la información de tu perfil.</p>
+        </div>
+    )
+}
+
+export default Perfil;
