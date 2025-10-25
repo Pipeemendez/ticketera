30e96468b6d3959556b2b87260254fcc3e0a7744
Felipe Méndez <94472095+Pipeemendez@users.noreply.github.com>
2025-10-25 18:17:44 -0300

    Update Navbar.jsx

--- front-end/src/components/Navbar/Navbar.jsx
+++ front-end/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,23 @@
 import './Navbar.css';
 import { FaShoppingCart } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
 
 function Navbar () {
     return (
         <nav className="navbar">
-            <div className='navbar-elemento'> 
-                Ticketera </div>
-            <div className='navbar-elemento'> 
-                Búsqueda </div>
-            <div className='navbar-elemento'>
-                <FaShoppingCart size={24} color="black" />
+            <div className='navbar-section-1'> 
+                Ticketera 
+            </div>
+            <div className='navbar-busqueda'> 
+                Búsqueda
+            </div>
+            <div className='navbar-section-2'>   
+                <div>
+                    <CgProfile />
+                </div>
+                <div>
+                    <FaShoppingCart size={24} color="black" />
+                </div>
             </div>
         </nav>
     )
