d6e6957c07a22945ec0d529a2bf97fcf59d723be
felipe <pipemendezri@gmail.com>
2025-10-27 17:15:00 -0300

    Update Evento.jsx

--- front-end/src/components/Main/Evento/Evento.jsx
+++ front-end/src/components/Main/Evento/Evento.jsx
@@ -1,12 +1,27 @@
 import "./Evento.css";
 import Boton from "../../Boton/Boton.jsx";
+import { useState } from "react";
 
 function Evento ({evento}) {
+
+    const [infoEvento, setInfoEvento] = useState(false);
+
     return (
         <li className="cartelera-item">
             <h4 className="cartelera-name"> {evento.nombre} </h4>
             <p className="cartelera-details"> {evento.fecha} | Lugar: {evento.lugar} | Precio: ${evento.precio} </p>
-            <Boton info = "Ver más información" />
+            <Boton info = "Ver más información" onClick={() => {
+                setInfoEvento(true);
+            }}/>
+            {infoEvento && (
+                <div className="cartelera-info-visible">
+                    <h4 className="cartelera-name"> {evento.nombre} </h4>
+                    <p className="cartelera-details"> {evento.fecha} | Lugar: {evento.lugar} | Precio: ${evento.precio} </p>
+                    <Boton info = "Cerrar" onClick={() => {
+                        setInfoEvento(false);
+                    }}/>
+                </div>
+            )}
         </li>   
     );
 }
