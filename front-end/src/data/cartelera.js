2af748b1632f60965879272eebb073ce0ad37313
felipe <pipemendezri@gmail.com>
2025-10-27 17:38:54 -0300

    Update cartelera.js

--- front-end/src/data/cartelera.js
+++ front-end/src/data/cartelera.js
@@ -1,16 +1,16 @@
 const eventos = [
-    {id:1, nombre:"Los Jaivas", fecha:"2023-10-15", lugar:"Estadio Nacional", precio:50},
-    {id:2, nombre:"Mon Laferte", fecha:"2023-11-20", lugar:"Teatro Caupolicán", precio:40},
-    {id:3, nombre:"Coldplay", fecha:"2024-01-10", lugar:"Estadio Monumental", precio:100},
-    {id:4, nombre:"BTS", fecha:"2024-02-25", lugar:"Movistar Arena", precio:120},
-    {id:5, nombre:"Shakira", fecha:"2024-03-15", lugar:"Estadio Nacional", precio:80},
-    {id:6, nombre:"Metallica", fecha:"2024-04-05", lugar:"Estadio Monumental", precio:110},
-    {id:7, nombre:"Dua Lipa", fecha:"2024-05-20", lugar:"Teatro Caupolicán", precio:70},
-    {id:8, nombre:"The Weeknd", fecha:"2024-06-30", lugar:"Movistar Arena", precio:90},
-    {id:9, nombre:"Adele", fecha:"2024-07-25", lugar:"Estadio Nacional", precio:130},
-    {id:10, nombre:"Lucky Brown", fecha:"2024-08-15", lugar:"Estadio Monumental", precio:85},
-    {id:11, nombre:"Imagine Dragons", fecha:"2024-09-10", lugar:"Teatro Caupolicán", precio:95},
-    {id:12, nombre:"Billie Eilish", fecha:"2024-10-05", lugar:"Movistar Arena", precio:105}
+    {id:1, nombre:"Los Jaivas", fecha:"2023-10-15", lugar:"Estadio Nacional", precio:50, entradasDisponibles:1500},
+    {id:2, nombre:"Mon Laferte", fecha:"2023-11-20", lugar:"Teatro Caupolicán", precio:40, entradasDisponibles:800},
+    {id:3, nombre:"Coldplay", fecha:"2024-01-10", lugar:"Estadio Monumental", precio:100, entradasDisponibles:2000},
+    {id:4, nombre:"BTS", fecha:"2024-02-25", lugar:"Movistar Arena", precio:120, entradasDisponibles:2500},
+    {id:5, nombre:"Shakira", fecha:"2024-03-15", lugar:"Estadio Nacional", precio:80, entradasDisponibles:1800},
+    {id:6, nombre:"Metallica", fecha:"2024-04-05", lugar:"Estadio Monumental", precio:110, entradasDisponibles:2200},
+    {id:7, nombre:"Dua Lipa", fecha:"2024-05-20", lugar:"Teatro Caupolicán", precio:70, entradasDisponibles:900},
+    {id:8, nombre:"The Weeknd", fecha:"2024-06-30", lugar:"Movistar Arena", precio:90, entradasDisponibles:1600},
+    {id:9, nombre:"Adele", fecha:"2024-07-25", lugar:"Estadio Nacional", precio:130, entradasDisponibles:1400},
+    {id:10, nombre:"Lucky Brown", fecha:"2024-08-15", lugar:"Estadio Monumental", precio:85, entradasDisponibles:1700},
+    {id:11, nombre:"Imagine Dragons", fecha:"2024-09-10", lugar:"Teatro Caupolicán", precio:95, entradasDisponibles:1100},
+    {id:12, nombre:"Billie Eilish", fecha:"2024-10-05", lugar:"Movistar Arena", precio:105, entradasDisponibles:1300},
 ]
 
 export default eventos;
