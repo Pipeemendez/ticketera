7cede1fe91afe9571ad7c6448872a1e8c1288fcc
felipe <pipemendezri@gmail.com>
2025-10-28 19:01:11 -0300

    Update main.jsx

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import App from './App.jsx';
 import Perfil from './pages/Perfil/Perfil.jsx';
+import Login from './pages/Login/Login.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/inicio" element={<App />} />
         <Route path='/perfil' element={<Perfil />} />
       </Routes>
     </BrowserRouter>
