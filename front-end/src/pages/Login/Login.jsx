2cefdc136918fdf4739e816845d1add2c4245fe0
felipe <pipemendezri@gmail.com>
2025-10-28 19:17:21 -0300

    Update Login.jsx

--- front-end/src/pages/Login/Login.jsx
+++ front-end/src/pages/Login/Login.jsx
@@ -1,12 +1,15 @@
 import './Login.css';
 import Footer from '../../components/Footer/Footer';
+import NavbarLogo from '../../components/Navbar/NavbarLogo/NavbarLogo';
+import LoginForm from './LoginForm/LoginForm';
 
 function Login() {
     return (
         <>
-        <div className="login-container">
-            login
+        <div className="navbar-login">
+            <NavbarLogo />
         </div>
+        <LoginForm />
         <Footer />
         </>
     )
