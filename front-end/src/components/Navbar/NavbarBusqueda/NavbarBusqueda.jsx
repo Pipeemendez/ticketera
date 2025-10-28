fad5a763966fe2326389d453ba282419fa37ecae
felipe <pipemendezri@gmail.com>
2025-10-28 19:01:16 -0300

    Create NavbarBusqueda.jsx

---
+++ front-end/src/components/Navbar/NavbarBusqueda/NavbarBusqueda.jsx
@@ -0,0 +1,11 @@
+import './NavbarBusqueda.css';
+
+function NavbarBusqueda () {
+    return (
+        <div className='busqueda'> 
+            Búsqueda
+        </div>
+    )
+}
+
+export default NavbarBusqueda;
