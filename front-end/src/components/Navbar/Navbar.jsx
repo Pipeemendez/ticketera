b99e24be237b35fe4f85fcd76c7c7066544dbc36
felipe <pipemendezri@gmail.com>
2025-10-28 19:01:23 -0300

    Update Navbar.jsx

--- front-end/src/components/Navbar/Navbar.jsx
+++ front-end/src/components/Navbar/Navbar.jsx
@@ -1,25 +1,14 @@
 import './Navbar.css';
-import { FaShoppingCart } from "react-icons/fa";
-import { CgProfile } from "react-icons/cg";
-import { Link } from "react-router-dom";
+import NavbarLogo from './NavbarLogo/NavbarLogo.jsx';
+import NavbarBusqueda from './NavbarBusqueda/NavbarBusqueda.jsx';
+import NavbarActions from './NavbarActions/NavbarActions.jsx';
 
 function Navbar () {
     return (
         <nav className="navbar">
-            <div className='navbar-section-1'> 
-                <Link className='link' to="/"> Ticketera </Link> 
-            </div>
-            <div className='navbar-busqueda'> 
-                Búsqueda
-            </div>
-            <div className='navbar-section-2'>   
-                <div>
-                    <Link className='link' to="/perfil"> <CgProfile /> </Link>
-                </div>
-                <div>
-                    <FaShoppingCart size={24} color="black" />
-                </div>
-            </div>
+            <NavbarLogo />
+            <NavbarBusqueda />
+            <NavbarActions />
         </nav>
     )
 }
