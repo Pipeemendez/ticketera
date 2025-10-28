0b36e2301327359dc0afe47b9d08797a1331f8f4
felipe <pipemendezri@gmail.com>
2025-10-28 19:01:07 -0300

    Create Login.jsx

---
+++ front-end/src/pages/Login/Login.jsx
@@ -0,0 +1,15 @@
+import './Login.css';
+import Footer from '../../components/Footer/Footer';
+
+function Login() {
+    return (
+        <>
+        <div className="login-container">
+            login
+        </div>
+        <Footer />
+        </>
+    )
+}
+
+export default Login;
